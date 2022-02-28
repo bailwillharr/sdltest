320eafe39ce3d50d0fccd6ab1d995a5bcf63812e
bailwillharr <baileyharrison04@outlook.com>
2022-02-28 14:29:29 +0000

    Change stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
+#include <SDL2/SDL.h>
+
 #include <iostream>
+#include <memory>
+
+class Window {
+private:
+	SDL_Window *m_handle;
+public:
+	Window(int width, int height);
+	~Window();
+};
+
+Window::Window(int width, int height)
+{
+	m_handle = SDL_CreateWindow("window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+}
+
+Window::~Window()
+{
+	SDL_DestroyWindow(m_handle);
+}
 
 int main()
 {
 
-	std::cout << "Hello world!\n";
+	std::unique_ptr<Window> win(new Window(640, 480));
+
+	for (;;);
 
 	return 0;
 
