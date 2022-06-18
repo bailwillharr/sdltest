f65ba03731c48faecbafa4cd382820379da5dffa
bailwillharr <baileyharrison04@outlook.com>
2022-06-18 10:26:09 +0100

    Write font code

--- src/components/ui.cpp
+++ src/components/ui.cpp
@@ -13,14 +13,18 @@ namespace components {
 
 UI::UI(Object* parent) : Component(parent, TypeEnum::UI)
 {
+	const std::string FONTFILE{ "LiberationMono-Regular.ttf" };
+
 	FT_Library library;
-	if (FT_Init_FreeType(&library)) {
+	FT_Face face;
+	int err;
+
+	err = FT_Init_FreeType(&library);
+	if (err) {
 		throw std::runtime_error("Failed to initialise freetype library");
 	}
 
-	FT_Face face;
-	const std::string FONTFILE { "LiberationMono-Regular.ttf" };
-	int err = FT_New_Face(library, parent->res.getFilePath(FONTFILE).string().c_str(), 0, &face);
+	err = FT_New_Face(library, parent->res.getFilePath(FONTFILE).string().c_str(), 0, &face);
 	if (err == FT_Err_Unknown_File_Format) {
 		FT_Done_FreeType(library);
 		throw std::runtime_error("Unknown file format for font '" + FONTFILE + "'");
@@ -30,7 +34,21 @@ UI::UI(Object* parent) : Component(parent, TypeEnum::UI)
 		throw std::runtime_error("Unable to open font 'font.ttf'");
 	}
 
-	std::cerr << "Number of glyphs: " << face->num_glyphs << "\n";
+	err = FT_Set_Pixel_Sizes(face, 0, 64);
+	if (err) {
+		FT_Done_Face(face);
+		FT_Done_FreeType(library);
+		throw std::runtime_error("Attempt to set pixel size to one unavailable in the font");
+	}
+
+	err = FT_Load_Char(face, 'H', FT_LOAD_RENDER);
+	if (err) {
+		FT_Done_Face(face);
+		FT_Done_FreeType(library);
+		throw std::runtime_error("Unable to load char glyph");
+	}
+
+
 
 	FT_Done_Face(face);
 	FT_Done_FreeType(library);
